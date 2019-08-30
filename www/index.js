7cf1bf3a67116f1460d6b34a6909c0e3afe04f4a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-08-30 11:55:54 +0200

    Reformats

--- www/index.js
+++ www/index.js
@@ -1,5 +1,5 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec')
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Global-121-Indy', 'coolMethod', [arg0]);
-};
+  exec(success, error, 'Global-121-Indy', 'coolMethod', [arg0])
+}

34d62b4f5263cf05699d6a995bdc494fea9199c5
Mark Spanbroek <mark@spanbroek.net>
2019-09-05 11:29:31 +0200

    Remove unnecessary argument

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,6 @@
 exports.defineAutoTests = function () {
   it('opens and closes wallet', done => {
     Global121.Indy.openWallet(
-      null,
       handle => {
         expect(handle).toBeDefined()
         Global121.Indy.closeWallet(handle)
--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec')
 
-exports.openWallet = function (arg0, success, error) {
-  exec(success, error, 'Global121Indy', 'openWallet', [arg0])
+exports.openWallet = function (success, error) {
+  exec(success, error, 'Global121Indy', 'openWallet', [])
 }
 
 exports.closeWallet = function (arg0, success, error) {
